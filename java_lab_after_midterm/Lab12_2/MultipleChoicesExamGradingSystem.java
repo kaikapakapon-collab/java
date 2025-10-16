8bbc82ed0f2051636bfa2744337720d8b5b700a7
Chinny <kaikapakapon@gmail.com>
2025-10-16 14:08:41 +0700

    Update MultipleChoicesExamGradingSystem.java

--- java_lab_after_midterm/Lab12_2/MultipleChoicesExamGradingSystem.java
+++ java_lab_after_midterm/Lab12_2/MultipleChoicesExamGradingSystem.java
@@ -1,24 +1,30 @@
-package Lab12_1;
-
-public class MultipleChoicesExamGradingSystem {
+public class GradeExam {
     public static void main(String[] args) {
+        // คำตอบของนักศึกษา (ตามตัวอย่างในเอกสาร)
         char[][] answers = {
-            {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
-            {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
-            {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
-            {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
-            {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
-            {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
-            {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
-            {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
+            {'A','B','A','C','C','D','E','E','A','D'},
+            {'D','B','A','B','C','A','E','E','A','D'},
+            {'E','D','D','A','C','B','E','E','A','D'},
+            {'C','B','A','E','D','C','E','E','A','D'},
+            {'A','B','D','C','C','D','E','E','A','D'},
+            {'B','B','E','C','C','D','E','E','A','D'},
+            {'B','B','A','C','C','D','E','E','A','D'},
+            {'E','B','E','C','C','D','E','E','A','D'}
         };
-        char[] correctAnswers = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
 
-        for(int nthStudent = 0; nthStudent < answers.length; nthStudent++){
-            int correctCount = 0;
-            for(int nthQuestion = 0; nthQuestion < answers[nthStudent].length; nthQuestion++)
-                if(answers[nthStudent][nthQuestion] == correctAnswers[nthQuestion]) correctCount++;
-            System.out.println("Student " + nthStudent + "'s correct count is " + correctCount);
+        // เฉลย (กรอกให้ตรงกับ Picture 2 ของอาจารย์)
+        // ถ้าอาจารย์ใช้คีย์มาตรฐาน มักเป็นชุดนี้:
+        char[] keys = {'D','B','D','C','C','D','A','E','A','D'};
+
+        // ตรวจและพิมพ์ผลคะแนนเหมือนตัวอย่าง
+        for (int i = 0; i < answers.length; i++) {
+            int correct = 0;
+            for (int j = 0; j < answers[i].length; j++) {
+                if (answers[i][j] == keys[j]) {
+                    correct++;
+                }
+            }
+            System.out.println("Student " + i + "'s correct count is " + correct);
         }
     }
 }
